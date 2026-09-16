adbe8e18309832d9df252dc6a29c1e28b27908be
C-Cipher404 <cortney.anderson4873@stu.mtec.edu>
2026-09-16 12:17:35 -0600

    more updates

--- PickersLab/PickersLab/ContentView.swift
+++ PickersLab/PickersLab/ContentView.swift
@@ -7,17 +7,81 @@
 
 import SwiftUI
 
+struct Park: Hashable {
+    var name: String
+    var yearBecameNationalPark: Int
+    var acres: Int
+    var closestTown: String
+    
+    var yearsAsNationalPark: Int {
+        2026 - yearBecameNationalPark
+    }
+}
+
 struct ContentView: View {
+    
+    static let zion = Park(
+    name: "Zion",
+    yearBecameNationalPark: 1900,
+    acres: 150000,
+    closestTown: "St. Geroge"
+        )
+    
+    static let bryceCanyon = Park(
+        name: "Bryce Canyon",
+        yearBecameNationalPark: 1900,
+        acres: 50000,
+        closestTown: "Bryce Canyon City"
+    )
+    
+    static let arches = Park(
+        name: "Arches",
+        yearBecameNationalPark: 1900,
+        acres: 80000,
+        closestTown: "Moab"
+        )
+    static let capitolReef = Park (
+        name: "Capitol Reef",
+        yearBecameNationalPark: 1900,
+        acres: 250000,
+        closestTown: "Torrey"
+        )
+    
+    @State private var selectedPark = ContentView.zion
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Picker ("Park", selection: $selectedPark){
+                
+                Text(ContentView.zion.name)
+                    .tag(ContentView.zion)
+                
+                Text(ContentView.bryceCanyon.name)
+                    .tag(ContentView.bryceCanyon)
+                
+                Text(ContentView.arches.name)
+                    .tag(ContentView.arches)
+                
+                Text(ContentView.capitolReef.name)
+                    .tag(ContentView.capitolReef)
+            }
+            .pickerStyle(.segmented)
+            
+            Text("Park: \(selectedPark.name)")
+            
+            Text("Became a National Park: \(selectedPark.yearBecameNationalPark)")
+            
+            Text("Acres: \(selectedPark.acres)")
+            
+            Text("Closest Town: \(selectedPark.closestTown)")
+            
+            Text("Years as a National Park: \(selectedPark.yearsAsNationalPark)")
         }
         .padding()
     }
-}
+    
+    }
+
 
 #Preview {
     ContentView()
--- Protocols/Protocols/ContentView.swift
+++ Protocols/Protocols/ContentView.swift
@@ -23,18 +23,3 @@ struct ContentView: View {
     ContentView()
 }
 
-//1.1
-// a. Has to have a battery percent
-// b. asking is it pulug in ? t/f
-// c. the a function to see if its plugged in to the charger
-
-// The bool statement could be a let becasue we will never change the value
-
-//1.2
-// isPluggedIn is a let constant, but the Chargeable protocol requires it to be var
-
-// ElectricScooter is missing the plugIn() function required by Chargeable.
-
-//1.3
-// can() needs to return a String.
-// ParkingPass is missing : Scannable.
